fa7d8134b7e7dc04283eab02511c8699e090786f
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-08-18 10:05:27 +0200

    last lecture from part3 - eslint

--- index.js
+++ index.js
@@ -1,118 +1,118 @@
-require("dotenv").config();
-const { Router, request, response } = require("express");
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const Note = require("./models/note");
+require("dotenv").config()
+const { Router, request, response } = require("express")
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const Note = require("./models/note")
 
 //Start app
-const app = express();
+const app = express()
 
 //Needed
-app.use(cors());
-app.use(express.static("build"));
-app.use(express.json());
+app.use(cors())
+app.use(express.static("build"))
+app.use(express.json())
 
 //Request logs
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
-app.use(requestLogger);
+  console.log("Method:", request.method)
+  console.log("Path:  ", request.path)
+  console.log("Body:  ", request.body)
+  console.log("---")
+  next()
+}
+app.use(requestLogger)
 
 //Post note
 app.post("/api/notes", (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ error: "content missing" });
+    return response.status(400).json({ error: "content missing" })
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-  });
+  })
   note
     .save()
     .then((savedNote) => {
-      response.json(savedNote);
+      response.json(savedNote)
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 //Fetching notes (all/single)
 app.get("/api/notes", (request, response, next) => {
   Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
+    response.json(notes)
+  })
+})
 
 app.get("/api/notes/:id", (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 //Updating importance
 app.put("/api/notes/:id", (req, res, next) => {
-  const id = req.params.id;
-  const body = req.body;
+  const id = req.params.id
+  const body = req.body
 
   const boilerNote = {
     content: body.content,
     important: body.important,
-  };
+  }
 
   Note.findByIdAndUpdate(id, boilerNote, { new: true })
     .then((updatedNote) => {
-      res.json(updatedNote);
+      res.json(updatedNote)
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 //Deleting note handler
 app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then((deletedNote) => {
-      response.status(204).end();
+      response.status(204).end()
     })
     .catch((err) => {
-      next(err);
-    });
-});
+      next(err)
+    })
+})
 
 //Handler for unknown endpoints
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: "unknown endpoint" })
+}
 
 //Handler for requests with result of errors
 const errorHandler = (err, req, res, next) => {
-  console.error(err.message);
+  console.error(err.message)
 
   if (err.name === "CastError") {
-    res.status(400).send({ err: "malformated id syntax" });
+    res.status(400).send({ err: "malformated id syntax" })
   } else if (err.name === "ValidationError") {
-    res.status(400).json({error: err.message})
+    res.status(400).json({ error: err.message })
   }
 
-  next(err);
-};
+  next(err)
+}
 
-app.use(unknownEndpoint);
-app.use(errorHandler);
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/note.js
+++ models/note.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
+console.log("connecting to", url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => {
-    console.log('connected to MongoDB')
+    console.log("connected to MongoDB")
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    console.log("error connecting to MongoDB:", error.message)
   })
 
 const noteSchema = new mongoose.Schema({
@@ -25,7 +25,7 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 })
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -33,4 +33,4 @@ noteSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema)
