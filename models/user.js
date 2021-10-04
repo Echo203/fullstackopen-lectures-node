367cdddea5eecde655a850c6a10de1a1935a4004
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-10-04 18:58:22 +0200

    Typos + reference in Note Collection

--- models/user.js
+++ models/user.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    name: String,
-    passwordHash: String,
-    notes:[
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Note'
-    ],
+  username: String,
+  name: String,
+  passwordHash: String,
+  notes:[
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note'
+    }
+  ],
 })
 
 userSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-        delete returnedObject.passwordHash
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
 })
 
 const User = mongoose.model('User', userSchema)
