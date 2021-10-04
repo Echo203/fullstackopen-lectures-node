8c73980925595e961ff7e8fcfd5bc123cf3b89a2
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-10-04 19:31:09 +0200

    Installing bcrypt + Users router configuration

---
+++ controllers/users.js
@@ -0,0 +1,21 @@
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+usersRouter.post('/', async (req, res) => {
+    const body = req.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+    res.json(user)
+})
+
+module.exports = usersRouter
