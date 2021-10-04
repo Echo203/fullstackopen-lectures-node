57caf0a42e480a57a92843591dc4226ba8c6fbd1
Echo203 <75540650+Echo203@users.noreply.github.com>
2021-10-04 19:32:00 +0200

    typos in users router

--- controllers/users.js
+++ controllers/users.js
@@ -3,19 +3,19 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
 usersRouter.post('/', async (req, res) => {
-    const body = req.body
+  const body = req.body
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-    const user = new User({
-        username: body.username,
-        name: body.name,
-        passwordHash,
-    })
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  })
 
-    const savedUser = await user.save()
-    res.json(user)
+  const savedUser = await user.save()
+  res.json(savedUser)
 })
 
 module.exports = usersRouter
